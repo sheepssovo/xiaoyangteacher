87774b38dd7381f20005597a3f440561009f4ba8
sheepssovo <tcbb6hmb82@privaterelay.appleid.com>
2026-06-03 19:09:58 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let idx=Math.floor(Math.random()*words.length);
-function render(){
-const w=words[idx];
-wordBox.innerHTML=`<h3>${w.word}</h3><div>${w.phonetic}</div><p><b>${w.meaning}</b></p><p>${w.example}</p><p>${w.translation}</p>`;
-book.innerHTML=(JSON.parse(localStorage.getItem('book')||'[]')).map(x=>'<div>'+x.word+' - '+x.meaning+'</div>').join('');
-}
-function nextWord(){idx=Math.floor(Math.random()*words.length);render();}
-function saveWord(){let b=JSON.parse(localStorage.getItem('book')||'[]');b.push(words[idx]);localStorage.setItem('book',JSON.stringify(b));render();}
-render();
